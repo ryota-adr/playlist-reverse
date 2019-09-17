01dd5b084b3cf2c3927380ce7ae73b2e0ea585ac
ryota_adr <pikyapikya12345@yahoo.co.jp>
2019-09-17 20:14:15 +0900

    インスタンス追加

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 require_once('vendor/autoload.php');
 use App\Initializer;
+use App\Playlist;
 
 session_start();
 
 $initializer = new Initializer(__DIR__);
 $initializer->init();
 
-$client = new Google_Client();
-$client->setClientId(CLIENT_ID);
-$client->setClientSecret(CLIENT_SECRET);
-$client->setScopes('https://www.googleapis.com/auth/youtube');
-$client->setRedirectUri(REDIRECT);
 
-$youtube = new Google_Service_YouTube($client);
+$playlist = new Playlist('', youtubeFactory(CLIENT_ID, CLIENT_SECRET, SCOPE, REDIRECT));
+
+$playlist->outputHtml();
+?>
