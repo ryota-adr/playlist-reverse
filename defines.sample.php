fd4382dd422b1cb183d46279395681a2479727bd
ryota_adr <pikyapikya12345@yahoo.co.jp>
2019-09-22 13:32:31 +0900

    first commit

---
+++ defines.sample.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_ENV', ''); //local or other
+define('APP_HOST', '');
+define('CLIENT_ID', '');
+define('CLIENT_SECRET', '');
+define('SCOPE', 'https://www.googleapis.com/auth/youtube');
+define('REDIRECT', '');
